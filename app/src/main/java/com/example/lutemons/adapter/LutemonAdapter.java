97a98650cebdc56d7b723e9a8382ca6b952541da
Emilija <emilija.kurtinaityte@gmail.com>
2025-04-11 14:27:15 +0300

    Added Home Activity

--- app/src/main/java/com/example/lutemons/adapter/LutemonAdapter.java
+++ app/src/main/java/com/example/lutemons/adapter/LutemonAdapter.java
@@ -3,6 +3,7 @@ package com.example.lutemons.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -28,11 +29,31 @@ public class LutemonAdapter extends RecyclerView.Adapter<LutemonAdapter.LutemonV
     @Override
     public void onBindViewHolder(@NonNull LutemonViewHolder holder, int position){
         Lutemon l=lutemons.get(position);
-        holder.txtName.setText(l.getName()+"("+l.getColor()+")");
+        holder.txtName.setText(l.getName());
         holder.txtStats.setText("ATK: " + l.getAttackPower() + " | DEF: " + l.getDefense() +
-                " | HP: " + l.getHealth() + "/" + l.getMaxHealth() +
-                " | XP: " + l.getExperience());
+                "\nHealth Points: " + l.getHealth() + "/" + l.getMaxHealth() +
+                " \nXP: " + l.getExperience());
+        switch (l.getColor().toLowerCase()){
+            case "white":
+                holder.itemBackground.setBackgroundColor(0xFFEDEDED);
+                break;
+            case "green":
+                holder.itemBackground.setBackgroundColor(0xFFABCB82);
+                break;
+            case "pink":
+                holder.itemBackground.setBackgroundColor(0xFFEB97C4);
+                break;
+            case "orange":
+                holder.itemBackground.setBackgroundColor(0xFFE3A97A);
+                break;
+            case "black":
+                holder.itemBackground.setBackgroundColor(0xFF757474);
+                break;
+            default:
+                holder.itemBackground.setBackgroundColor(0xFFFFFFFF);
 
+        }
+        holder.imgLutemon.setImageResource(R.drawable.lutemon);
     }
     @Override
     public int getItemCount(){
@@ -40,10 +61,14 @@ public class LutemonAdapter extends RecyclerView.Adapter<LutemonAdapter.LutemonV
     }
     public static class LutemonViewHolder extends RecyclerView.ViewHolder{
         TextView txtName, txtStats;
+        View itemBackground;
+        ImageView imgLutemon;
         public LutemonViewHolder(@NonNull View itemView){
             super(itemView);
             txtName=itemView.findViewById(R.id.txtLutemonName);
             txtStats=itemView.findViewById(R.id.txtLutemonStats);
+            itemBackground=itemView.findViewById(R.id.itemBackground);
+            imgLutemon=itemView.findViewById(R.id.imageLutemon);
         }
     }
 }
